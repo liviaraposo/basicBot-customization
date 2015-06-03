d569b92509a7876fc3aecb9256d1c5e173715c0a
liviaraposo <livia.raposo@hotmail.com>
2015-06-03 16:20:38 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -17,17 +17,17 @@
 
         /*
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
-                    'deu-lhe um biscoito de aveia caseiro macio!',
+                    'deu-lhe um biscoito de merda',
                     'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
                     'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
                     'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
                     'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Fode essa buceta!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Lívia rainha suprema"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eta"',
                     'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Lívia rainha branca',
                     'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
                     'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
                     'deu-lhe um Oreo e um copo de leite.',
@@ -45,7 +45,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me 1, 2, 3... testando"); // Mensagem a ser enviada no chat
+                    API.sendChat("/Testando!"); // Mensagem a ser enviada no chat
                 }
             }
         };
@@ -58,7 +58,7 @@
     //Mudar as configurações padrões do bot e ter certeza que elas foram carregadas corretamente
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "Lili Bot",
         language: "portuguese",
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
@@ -77,32 +77,32 @@
         voteSkip: false,
         voteSkipLimit: 10,
         timeGuard: true,
-        maximumSongLength: 10,
+        maximumSongLength: 8,
         autodisable: true,
         commandCooldown: 30,
         usercommandsEnabled: true,
         lockskipPosition: 3,
         lockskipReasons: [
-        	["tema", "A música não se encaixa nos padrões da sala. "],
-        	["op", "Essa música está na lista OP. "],
+        	["caralho", "ngm gostou desse caralho"],
+        	["eta", "Mudei pq quis, eai???"],
         	["historico", "A música ainda está no histórico. "],
         	["mix", "Você tocou um mix (muito longo) - não permitido. "],
         	["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
         	["nsfw", "A música que você tocou é NSFW (impróprio). "],
-        	["indisponivel", "A música que você tocou está indisponível. "]
+        	["indisponivel", "A música que você tocou está indisponível porra. "]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 5,
-        motd: "Mensagem temporária do dia",
+        motd: "",
         filterChat: true,
         etaRestriction: false,
-        welcome: true,
+        welcome: Welcome/Bem vindo! %u,
         opLink: null,
-        rulesLink: null,
+        rulesLink: Regras aqui! https://attachment.fbsbx.com/file_download.php?id=1601180993495159&eid=ASst_Efv-SLfhLvkR2JA-BmocpxFlbov36QkFMBTGN2HnHtRvOgEZnO3eeyYTrm7RNA&inline=1&ext=1433359020&hash=AStXonmQvc1006o9,
         themeLink: null,
-        fbLink: null,
+        fbLink: Curta nossos parceiros no FaceBook: http://senta.la/1r34x (e) http://senta.la/1r34y,
         youtubeLink: null,
         website: null,
         intervalMessages: [],
@@ -110,8 +110,7 @@
         songstats: true,
         commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json"
+          
         }
     }));
 
